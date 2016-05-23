439002ad19d4d512a5543e5f33772fc2beeb71bc
scoin <greg.piccolo@gmail.com>
2016-05-23 17:18:02 -0400

    test routes for services

--- api/routes/users/users.js
+++ api/routes/users/users.js
@@ -3,5 +3,12 @@ module.exports = {
 		res.status(200).json({
 			id: req.params.id
 		})
+	},
+	getName: function(req, res){
+		var globals = this;
+		res.status(200).json({
+			name: globals.services.user.get(req.params.name),
+			address: globals.services.user.address()
+		})
 	}
 }
