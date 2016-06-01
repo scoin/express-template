83ca0cafc3001890f9fc7675a45cd49cb471b9c9
scoin <greg.piccolo@gmail.com>
2016-06-01 14:13:04 -0400

    rename of discovery module

--- config/afterStart.js
+++ config/afterStart.js
@@ -1,7 +1,7 @@
 //any operations for after server start go here
 
 module.exports = function(address, port){
-	this.dnb.discovery.announce(this.name, {
+	this.discovery.announce(this.name, {
 		protocol: "http",
 		host: address.address,
 		port: address.port,
