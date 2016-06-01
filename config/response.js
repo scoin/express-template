516a91e92df49e5601896c103ec5518c79f2eb39
scoin <greg.piccolo@gmail.com>
2016-06-01 12:16:50 -0400

    move error handling into custom responses config file

--- config/response.js
+++ config/response.js
@@ -1,15 +1,30 @@
-function errorHandler(err, message){
+var analytics = require('dnb-common').analytics;
+
+function errorHandler(req, res, err, message){
 	console.error(err);
 	console.error(message);
-
-	return {
+	var data = {
 		error: this.error,
 		message: message || err.message,
 		statusCode: this.statusCode
+	};
+	res.status(this.statusCode).json(data);
+	if(process.env.LOG_REQUESTS){
+		analytics.log(req, data);
 	}
 }
 
-module.exports = {
+function successHandler(req, res, data){
+	res.status(this.statusCode).json(data);
+	if(process.env.LOG_REQUESTS){
+		analytics.log(req, data);
+	}
+}
+
+var responses = {
+	ok: successHandler.bind({statusCode: 200}),
+	created: successHandler.bind({statusCode: 201}),
+	accepted: successHandler.bind({statusCode: 202}),
 	badRequest: errorHandler.bind({statusCode: 400, error: "BAD REQUEST"}),
 	unauthorized: errorHandler.bind({statusCode: 401, error: "UNAUTHORIZED"}),
 	forbidden: errorHandler.bind({statusCode: 403, error: "FORBIDDEN"}),
@@ -17,4 +32,10 @@ module.exports = {
 	tooLarge: errorHandler.bind({statusCode: 413, error: "REQUEST ENTITY TOO LARGE"}),
 	unsupportedMediaType: errorHandler.bind({statusCode: 415, error: "UNSUPPORTED MEDIA TYPE"}),
 	serverError: errorHandler.bind({statusCode: 500, error: "SERVER ERROR"}),
+}
+
+module.exports = function(req, res){
+	for(var resp in responses){
+		res[resp] = responses[resp].bind(null, req, res);
+	}
 }
