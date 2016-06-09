13bbb22ea8df0cf0c56eab7b2099782068a24f96
scoin <greg.piccolo@gmail.com>
2016-06-09 16:34:22 -0400

    negotiate error response for easier error handling

--- config/response.js
+++ config/response.js
@@ -1,11 +1,13 @@
 var analytics = require('dnb-common').analytics;
 var nconf = require("nconf");
+var _ = require('lodash');
 
 function errorHandler(req, res, err, message){
 	console.error(err);
 	console.error(message);
 	var data = {
-		error: this.error,
+		responseText: this.error,
+		error: _.get(err, "body", {}),
 		message: message || err.message,
 		statusCode: this.statusCode
 	};
@@ -13,6 +15,20 @@ function errorHandler(req, res, err, message){
 	if(nconf.get("LOG_REQUESTS")){
 		analytics.log(req, data);
 	}
+}
+
+function negotiate(req, res, err, message, defaultStatus){
+	if(!defaultStatus){
+		defaultStatus = 500;
+	}
+
+	status = _.get(err, "statusCode", defaultStatus).toString();
+	//search for more statuses and make smart decisions
+
+	if(statusCodeMap[status]){
+		return statusCodeMap[status](req, res, err, message);
+	}
+	return responses.serverError(req, res, err, message);
 }
 
 function successHandler(req, res, data){
@@ -26,7 +42,7 @@ function successHandler(req, res, data){
 	} else {
 		resp.data = data;
 	}
-	res.status(this.statusCode).json(resp);
+	res.status(this.statusCode).json(resp.body);
 	if(nconf.get("LOG_REQUESTS")){
 		analytics.log(req, resp);
 	}
@@ -43,6 +59,20 @@ var responses = {
 	tooLarge: errorHandler.bind({statusCode: 413, error: "REQUEST ENTITY TOO LARGE"}),
 	unsupportedMediaType: errorHandler.bind({statusCode: 415, error: "UNSUPPORTED MEDIA TYPE"}),
 	serverError: errorHandler.bind({statusCode: 500, error: "SERVER ERROR"}),
+	negotiate: negotiate
+}
+
+var statusCodeMap = {
+	"200": responses.ok,
+	"201": responses.created,
+	"202": responses.accepted,
+	"400": responses.badRequest,
+	"401": responses.unauthorized,
+	"403": responses.forbidden,
+	"404": responses.notFound,
+	"413": responses.tooLarge,
+	"415": responses.unsupportedMediaType,
+	"500": responses.serverError,
 }
 
 module.exports = function(req, res){
